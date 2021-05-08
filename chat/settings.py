1574b56266e9538cde4452987031ba2a5051ecc1
Amruthalingam-k <amruthalingam96@gmail.com>
2021-05-08 20:45:50 +0530

    env

--- chat/settings.py
+++ chat/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'sx(kww!*9ct8fb3d2ic-$%93ln)fm9gik)%bb=#wzmc_6yj%vi'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
